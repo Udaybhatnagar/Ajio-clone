750369f3700b55e7e1cbc7e71ea882fbb2ac479e
Uday Bhatnagar <udaybhatnagar502@gmail.com>
2025-06-16 15:44:38 +0530

    navbar creation

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,59 @@
 function Navbar(){
 
   return (
-    <div>
+    <div className="Navbar">
 
-      <h1>Ajio</h1>
-      <ul>
+      <h1 className="heading">AJIO</h1>
+      <ul className="list">
         <li>Men</li>
         <li>Women</li>
         <li>Kids</li>
+        <li>Cart</li>
       </ul>
 
     </div>
-
-    
-    
   )
 }
+
+const ProductCart= () =>{
+
+  return (
+    <div className="product-cart">
+       <Product/>
+        <Product/>
+         <Product/>
+          <Product/>
+           <Product/>
+            <Product/>
+             <Product/>
+              <Product/>
+               <Product/>
+
+    </div>
+  )
+}
+
+
+
+function Product(){
+return (
+  <div className="Product">
+    <img src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1739256402_8748785.jpg?w=480&dpr=1.3" alt="" className="tshirt" />
+    <h1>HRX Tshirt</h1>
+    <p>4.5 ratings</p>
+    <p>499 Rs</p>
+  </div>
+)
+
+}
+
 
 function App() {
 
   return (
     <>
     <Navbar/>
+    <ProductCart/>
     </>
   )
 }
