d8bc2f6d896afcc3ed54f6e2b017238d00306245
Uday Bhatnagar <udaybhatnagar502@gmail.com>
2025-06-16 15:12:14 +0530

    Ajio day 1

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+
+function Navbar(){
+
+  return (
+    <div>
+
+      <h1>Ajio</h1>
+      <ul>
+        <li>Men</li>
+        <li>Women</li>
+        <li>Kids</li>
+      </ul>
+
+    </div>
+
+    
+    
+  )
+}
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
+
+export default App
