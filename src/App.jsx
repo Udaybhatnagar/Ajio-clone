8ba5e8fb2ed10704b836707a45bfc312094c24f5
Uday Bhatnagar <udaybhatnagar502@gmail.com>
2025-06-21 14:27:03 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-
 import Navbar from "./components/Navbar";
 import ProductCart from "./components/ProductCart";
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-
-
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Kids } from "./components/Kids";
 
 function App() {
-
+  
   return (
-    <>
-    <Navbar/>
-    <ProductCart/>
-    </>
+  
+     <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navbar />} />
+      <Route path="/Kids" element={<Kids />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
-export default App
+export default App;
